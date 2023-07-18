8c673f567d6814599bbbcf51e31a5ff0666211d1
Mario201087 <mariusz201087@tlen.pl>
2023-07-18 13:55:29 +0200

    pictures

--- jpg.php
+++ jpg.php
@@ -1,3 +1,15 @@
+<?php
+
+include("connect.php");
+
+
+if($_SESSION['premissions']!=1)
+{
+  header("Location: index.php");
+}
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,36 +18,78 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            line-height: 1.4em;
+            vertical-align: baseline;
+            text-decoration: none;
+            outline: 0;
+            }
+    </style>
   </head>
   <body style="width: 100vw ;height: 100vh;">
-  <section class="container-fluid" style="background-color: #8fc4b7; height: 100vh;">
-  <div class="container py-5 h-100">
+   <div class="container-fluid " style="height:8%;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand col-2" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        </ul>
+    </div>
+    </nav>
+  </div>
+  <section class="container-fluid" style="background-color: #8fc4b7;height:92%;">
+  <div class="col12 h-25"></div>
+  <div class="container my-5">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-8 col-xl-6">
         <div class="card rounded-3">
           <div class="card-body p-4 p-md-5">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2 ">Registration Info</h3>
-            <form class="input-group ">
-                <div class="custom-file col-12">
-                    <input type="file" class="btn btn-outline-secondary col-12 p-1" id="inputGroupFile04">
+            <form action="picture.php" method="post" enctype="multipart/form-data">
+               <div class="row d-flex ">
+                    <div class="col p-1">
+                    <input type="text" class="form-control " name="name" placeholder="Email">
+                    </div>
                 </div>
-                <div class="form-group mt-2 col-12">
-                    <select class="form-control col-12" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </select>
+                <div class="row d-flex ">
+                    <div class="col p-1">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
                 </div>
-                <div class="form-group mt-2 col-12">
-                    <select class="form-control col-12" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </select>
+                <div class="row d-flex ">
+                    <div class="col p-1">
+                     <input type="file" class="col-12 btn btn-primary"  name="plik">
+                    </div>
+                </div>
+                <div class="row d-flex ">
+                    <div class="col p-1">
+                     <input type="submit" name="submit" class="btn btn-primary col-12" value="Upload">
+                    </div>
                 </div>
             </form>
           </div>
@@ -44,10 +98,6 @@
     </div>
   </div>
 </section>
-    <script>
-        flatpickr("#myID", {});
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-  </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
 </html>
